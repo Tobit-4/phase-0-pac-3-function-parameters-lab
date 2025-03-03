968a51d0dd3f39d6499d8742ed067fe4da55302a
Fidel <fidel.kipkoech@student.moringaschool.com>
2025-03-03 19:37:21 +0300

    made changes

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 function introduction(name){
-    console.log(`Hi, my name is ${name}.`);
+    return `Hi, my name is ${name}.`;
 }
-introduction(Aki);
+
 
 function introductionWithLanguage(name , language){
-    console.log(`Hi, my name is ${name} and am learning to program in ${language}.`);
+    return `Hi, my name is ${name} and I am learning to program in ${language}.`;
 }
-introductionWithLanguage(Aki, Ember.js);
+
 
 function introductionWithLanguageOptional(name, language="JavaScript"){
-    console.log(`Hi, my name is ${name} and am learning to program in ${language}.`);
+    return `Hi, my name is ${name} and am learning to program in ${language}.`;
 }
-introductionWithLanguageOptional(Gracie);
+
